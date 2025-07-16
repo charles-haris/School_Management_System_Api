86fbbe3822e224c63248fd261f66594c0682db40
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-07-16 17:06:46 +0000

    update on Academic_year model

--- academic_year/admin.py
+++ academic_year/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Academic_year
 
 # Register your models here.
+
+admin.site.register(Academic_year)
--- academic_year/models.py
+++ academic_year/models.py
@@ -10,4 +10,9 @@ class Academic_year(models.Model):
     year_name = models.CharField(max_length=20)
     start_date = models.DateField()
     end_date = models.DateField()
+    is_current = models.BooleanField(default=True)
+    status = models.CharField(max_length=10, choices=Status, default=Status.ACTIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
