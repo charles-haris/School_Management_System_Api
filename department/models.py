2baa4c74bb8658df863904a5645af8c47fd95f32
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-07-16 17:19:37 +0000

    department model created

--- department/models.py
+++ department/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from teacher.models import Teacher
 
 # Create your models here.
+class Status(models.TextChoices):
+    ACTIVE = "Active", "active"
+    INACTIVE = "Inactive", "inactive"
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=100)
+    department_uuid =  models.UUIDField(unique=True)
+    department_code =  models.CharField(max_length=10 ,unique=True)
+    head_of_department = models.ForeignKey(Teacher, on_delete = models.CASCADE)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=10, choices = Status, default= Status.ACTIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
